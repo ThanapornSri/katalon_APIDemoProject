0ff4c598ad800cb3caaef469eb4ac1a010df00d7
thanaporn.sri <thanaporn.sri@MSP016-0000.miki.solutions>
2019-03-04 16:03:22 +0700

    First project commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ca6633ba-c0bc-4ca0-8996-20183b40b32b</testSuiteGuid>
+   <testCaseLink>
+      <guid>de3ad7a0-5c6f-4929-a54d-d813b7b753bc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTest/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6b500d0-dee3-4765-bc56-ac85919a78ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2e483df-5299-47ed-b1e3-12169e95b0cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de8e0547-48e7-4398-86a2-1501e08b7935</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
